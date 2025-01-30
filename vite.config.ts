4e1ddc5cefc747c7bcac5206672e2ea0d11dac1d
Sebastian Vargas <svquintana98@gmail.com>
2025-01-29 22:44:21 -0500

    Fix base URL deploy

--- vite.config.ts
+++ vite.config.ts
@@ -8,15 +8,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
-  base: 'https://searinox0902.github.io/frontend-ux-test-vue/',
+  plugins: [vue(), vueDevTools(), tailwindcss()],
+  base: 'frontend-ux-test-vue/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
